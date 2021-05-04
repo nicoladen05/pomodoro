0fab57b377a2908bea0c4f5d313f3cc895b5d728
nicoladen05 <nicolashartmanntaba@gmail.com>
2021-05-04 09:02:05 +0200

    setTime function

--- pomodoro.py
+++ pomodoro.py
@@ -1,9 +1,11 @@
 from time import sleep
 
 class Pomodoro:
-    # Main timer function
-    def timer(self, hours, minutes, seconds):
+    def setTime(self, hours, minutes, seconds):
         self.seconds_total = seconds + minutes * 60 + hours * 3600
+
+    # Main timer function
+    def timer(self):
         while self.seconds_total > 0:
             self.seconds_total -= 1
             sleep(1)
@@ -12,7 +14,7 @@ class Pomodoro:
     def time(self):
         try:
             return self.seconds_total
-        except AttributeError: # If the pomodoro isn't running, return False
+        except AttributeError: # If the time isn't set yet
             return False
 
 p = Pomodoro()
