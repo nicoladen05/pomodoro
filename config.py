b64e8a0839374ae0f88fb9f9e4ffdfacaae7262a
nicoladen05 <nicolashartmanntaba@gmail.com>
2021-05-11 10:27:37 +0200

    removed old config lines

--- config.py
+++ config.py
@@ -2,5 +2,3 @@ studyTime = [0,25,0] # Study time [hours, minutes, seconds]
 breakTime = [0,5,0] # Break time [hours, minutes, seconds]
 longBreakTime = [0,15,0] # Long break time [hours, minutes, seconds]
 shortBreakAmount = 3 # Ammount of short breakes before a long one
-blockWebsites = True # Block websites when the timer is running
-websitesToBlock = ["youtube.com", "twitch.tv", "reddit.com"]
